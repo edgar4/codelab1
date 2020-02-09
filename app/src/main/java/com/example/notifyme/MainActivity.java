e5a97da29f905d359b47c661465323b4de549037
Edgar Chris <edgarchris4@gmail.com>
2020-02-09 10:40:52 +0300

    basic notification

---
+++ app/src/main/java/com/example/notifyme/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.notifyme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button_notify;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button_notify = findViewById(R.id.notify);
+        button_notify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public
+
+        })
+    }
+
+    public vode sendNotification() {
+
+    }
+}
