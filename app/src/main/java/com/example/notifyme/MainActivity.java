dffdc124663c9ae9bf199a68a288570938e376d5
Edgar Chris <edgarchris4@gmail.com>
2020-02-09 11:02:23 +0300

    show the notification priority

--- app/src/main/java/com/example/notifyme/MainActivity.java
+++ app/src/main/java/com/example/notifyme/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.notifyme;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
@@ -13,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     private Button button_notify;
     private static final String PRIMARY_CHANNEL_ID = "primary_notification_channel";
     private NotificationManager mNotifyManager;
+    private static final int NOTIFICATION_ID = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,13 +30,17 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        createNotificationChannel();
     }
 
     public void sendNotification() {
+        NotificationCompat.Builder notifyBuilder = getNotificationBuilder();
+        mNotifyManager.notify(NOTIFICATION_ID, notifyBuilder.build());
 
     }
-    public void createNotificationChannel()
-    {
+
+    public void createNotificationChannel() {
         mNotifyManager = (NotificationManager)
                 getSystemService(NOTIFICATION_SERVICE);
         if (android.os.Build.VERSION.SDK_INT >=
@@ -47,5 +55,21 @@ public class MainActivity extends AppCompatActivity {
             notificationChannel.setDescription("Notification from Mascot");
             mNotifyManager.createNotificationChannel(notificationChannel);
         }
+    }
+
+
+    private NotificationCompat.Builder getNotificationBuilder() {
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent notificationPendingIntent = PendingIntent.getActivity(this,
+                NOTIFICATION_ID, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        NotificationCompat.Builder notifyBuilder = new NotificationCompat.Builder(this, PRIMARY_CHANNEL_ID)
+                .setContentTitle("You've been notified!")
+                .setContentText("This is your notification text.")
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setDefaults(NotificationCompat.DEFAULT_ALL)
+                .setContentIntent(notificationPendingIntent)
+                .setAutoCancel(true)
+                .setSmallIcon(R.drawable.ic_android);
+        return notifyBuilder;
     }
 }
